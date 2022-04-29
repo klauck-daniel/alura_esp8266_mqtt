21066e0155a07cdd7eaffec4c23b915286ee5b4e
klauck-daniel <97921706+klauck-daniel@users.noreply.github.com>
2022-04-29 15:39:14 -0300

    Update do código .ino e adição do link do Notion para ver anotações.

--- alura_temperatura_mqtt.ino
+++ alura_temperatura_mqtt.ino
@@ -1,16 +1,20 @@
+// Projeto desenvolvido em curso da Alura.
+// Mais anotações no Notion: https://sturdy-ginger-704.notion.site/NodeMCU-e-Esp8266-medindo-e-publicando-dados-com-MQTT-cd892191162849f9828b1c689cfac337
+
+
 // --- WIFI ---
 #include <ESP8266WiFi.h>
-const char* ssid = "minhaRede";          // troque pelo nome da sua rede
-const char* password = "minhaSenha";     // troque pela senha da sua rede
+const char* ssid = "SSID_da_rede";          // troque pelo nome da sua rede
+const char* password = "senha_da_rede";     // troque pela senha da sua rede
 WiFiClient nodemcuClient;
 
 // --- MQTT ---
 #include <PubSubClient.h>
-const char* mqtt_Broker = "iot.eclipse.org";
-const char* mqtt_ClientID = "termometro-remerces01";
-PubSubClient client(nodemcuClient);
-const char* topicoTemperatura = "rmerces/temperatura";
-const char* topicoUmidade = "rmerces/umidade";
+const char* mqtt_Broker = "iot.eclipse.org";           //define o endereço do broker
+const char* mqtt_ClientID = "termometro-remerces01";   //define um nome amigável para o cliente MQTT
+PubSubClient client(nodemcuClient);                    //cria um objeto que deve estar associado a uma wifi
+const char* topicoTemperatura = "rmerces/temperatura"; //cria o tópico para publicar
+const char* topicoUmidade = "rmerces/umidade";         //cria o tópico para publicar
 
 // --- DHT ---
 #include <DHT.h>
@@ -25,21 +29,24 @@ int temperatura;
 #define OLED_RESET LED_BUILTIN
 Adafruit_SSD1306 display(OLED_RESET);
 
+// --- SETUP ---
 void setup() {
-  configurarDisplay();
-  conectarWifi();
-  client.setServer(mqtt_Broker, 1883);
+  configurarDisplay();                  //configura o display
+  conectarWifi();                       //conecta na wifi
+  client.setServer(mqtt_Broker, 1883);  //inicializa o servidor MQTT
 }
 
+// --- LOOP ---
 void loop() {
   if (!client.connected()) {
-    reconectarMQTT();
+    reconectarMQTT(); //caso não esteja conectado com o broker, irá chamar a função para conectar antes de executar o demais passos
   }
-  medirTemperaturaUmidade();
+  medirTemperaturaUmidade(); 
   mostrarTemperaturaUmidade();
   publicarTemperaturaUmidadeNoTopico();
 }
 
+//--- CONECTA NA WIFI ---
 void conectarWifi() {
   delay(10);
 
@@ -57,29 +64,34 @@ void conectarWifi() {
   }
 }
 
+//--- RECONECTA MQTT CLIENT ---
 void reconectarMQTT() {
   while (!client.connected()) {
-    client.connect(mqtt_ClientID);
+    client.connect(mqtt_ClientID); //função reconcta o MQTT caso a conexão caia
   }
 }
 
+//--- CONFIGURA DISPLAY ---
 void configurarDisplay() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setTextColor(WHITE);
   display.clearDisplay();
 }
 
+//--- PUBLICA (MQTT) TEMPERATURA E UMIDADE ---
 void publicarTemperaturaUmidadeNoTopico() {
-  client.publish(topicoTemperatura, String(temperatura).c_str(), true);
-  client.publish(topicoUmidade, String(umidade).c_str(), true);
+  client.publish(topicoTemperatura, String(temperatura).c_str(), true); //a ultima flag ("true") faz com que o broker exiba a ultima mensagem publicada
+  client.publish(topicoUmidade, String(umidade).c_str(), true); //a ultima flag ("true") faz com que o broker exiba a ultima mensagem publicada
 }
 
+// --- ONTÉM TEMPERATURA E UMIDADE---
 void medirTemperaturaUmidade() {
   umidade = dht.readHumidity();
   temperatura = dht.readTemperature(false);
   delay(5000);
 }
 
+// --- ESTRUTURA MENSAGEM PARA DISPLAY ---
 void mostrarTemperaturaUmidade() {
   mostrarMensagemNoDisplay("Temperatura", (temperatura), " C");
   mostrarMensagemNoDisplay("Umidade", (umidade), " %");
